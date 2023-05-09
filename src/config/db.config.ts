6db6b9218a8c73d61012568afef9e017213101c2
andriiZelinskyi <andry.zelinskiy@gmail.com>
2023-05-09 15:29:50 +0300

    delete express project

--- src/config/db.config.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-
-export const pool = new Pool({
-  user: process.env.NAME,
-  password: process.env.PASSWORD,
-  host: 'localhost',
-  port: 5432,
-  database: 're'
-});
--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-import { userRouter } from './components/user/user.routes';
-
-export const routes = express.Router();
-
-routes.use('/user', userRouter);
