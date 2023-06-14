94bc910ecda4d8ecb5bc2bed68e3941c1189ae2a
andriiZelinskyi <andry.zelinskiy@gmail.com>
2023-06-14 13:43:07 +0300

    prepare for production 1

--- src/features/notifications/firebase-admin.service.ts
+++ src/features/notifications/firebase-admin.service.ts
@@ -1,8 +1,6 @@
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
 
-import * as serviceAccount from './ermessaging-firebase-adminsdk-ntptg-18d4f97d24.json';
-
 admin.initializeApp({
   credential: admin.credential.cert({
     type: 'service_account',
